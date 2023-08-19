075ab8bd93c5922532e6fb004cd9983f5eb174f9
Loags <35934353+Loags@users.noreply.github.com>
2023-08-19 18:05:03 +0200

    chore(unity-version): - changed to newer unity version

--- ThirdPersonRPG/Assets/_Project/Scripts/Controls/PlayerControls.cs
+++ ThirdPersonRPG/Assets/_Project/Scripts/Controls/PlayerControls.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:dbb9e1bda949c16d2355a9b5bc693ac08196f7569ae8cad3daf64e486edeff21
-size 19551
+oid sha256:833f3a3ad91798357d9b75f8865f524fb4810c01dc01992b100463e6cfaacef9
+size 19732
