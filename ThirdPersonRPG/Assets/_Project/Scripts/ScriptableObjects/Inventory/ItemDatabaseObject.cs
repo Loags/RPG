a542b292c833e0761e120817ff4bca842a6d6175
Loags <35934353+Loags@users.noreply.github.com>
2024-06-20 14:16:38 +0200

    chore(refactor): - added onvalidate to ItemDatabaseObject.cs to reproduce the itemObject references if they have been lost

--- ThirdPersonRPG/Assets/_Project/Scripts/ScriptableObjects/Inventory/ItemDatabaseObject.cs
+++ ThirdPersonRPG/Assets/_Project/Scripts/ScriptableObjects/Inventory/ItemDatabaseObject.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:37eed10274905bec939b7ad1bf33fa1cdee6e2b13513ce892cde9ccc9d1585ec
-size 11107
+oid sha256:5c2581cb63a2de389207ae3db67a5a87658ae8d4c5a33d96230382df92dde187
+size 11180
